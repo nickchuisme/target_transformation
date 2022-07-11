de7e06ce83a864903b506b8556e540897daa6dbb
Chuchum <910253@gmail.com>
2022-07-11 17:05:49 +0100

    update setting

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 model_selection.py --thresholds 0.003 0.005 0.008 0.013 0.02 0.04 0.07 0.12 0.2 0.25 --lags 1 2 3 4 5 --data_num 32 --data_length 300 --worker 30
+python3 model_selection.py --thresholds 0.003 0.005 0.008 0.013 0.02 0.04 0.07 0.12 0.2 0.25 --lags 1 2 3 4 5 --gap 0 --data_num 32 --data_length 300 --worker 30
 
 echo "[[Finished!]]"
