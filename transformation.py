a68f3009d42cb6bd4bab26827c640905304c74e2
Chuchum <910253@gmail.com>
2022-07-25 10:42:20 +0100

    det return decomposed series

--- transformation.py
+++ transformation.py
@@ -1,18 +1,19 @@
 import numpy as np
+import pandas as pd
+import pywt
+from PyEMD import EMD
 from scipy import stats
 from scipy.special import inv_boxcox
-import pywt
-import pandas as pd
-from PyEMD import EMD
+from sklearn.preprocessing import MaxAbsScaler, MinMaxScaler, StandardScaler
+from statsmodels.stats.diagnostic import acorr_ljungbox
 
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import MaxAbsScaler
-from sklearn.preprocessing import StandardScaler
 
 class Transformation:
 
-    def __init__(self, series=None, transformation_list=[]):
+    def __init__(self, series=[], transformation_list=[]):
         self.series = series
+        self.series_len = len(series)
+        self.level = None
         self.boxcox_lambda = None
         self.transformation_list = transformation_list
 
@@ -81,19 +82,47 @@ class Transformation:
         self.series = np.array(series).ravel()
         return self.series
 
-    ## suggest 0.5 -> origin
-    def dwt(self, series, threshold=0.5, mode='smooth', wavelet='sym8', inverse=False, test=False):
+    def dwt(self, series, threshold=0.7, mode='smooth', wavelet='sym8', inverse=False, test=False):
         series = np.array(series).ravel()
-        coeffs = pywt.wavedec(data=series, wavelet=wavelet, mode=mode)
-        # coeffs[1:] = [pywt.threshold(i, value=threshold*np.mean(series), mode='soft') for i in coeffs[1:]]
+        self.level = pywt.dwt_max_level(self.series_len, wavelet)
+        coeffs = pywt.wavedec(data=series, wavelet=wavelet, mode=mode, level=self.level)
         threshold = threshold * (np.median(np.abs(coeffs[-1]))/0.6745) * np.sqrt(2 * np.log(len(coeffs[-1])))
-
-        # coeffs[1:] = [pywt.threshold(i, value=threshold * (np.median(np.abs(i))/0.6745) * np.sqrt(2 * np.log(len(i))), mode='hard') for i in coeffs[1:]]
         coeffs[1:] = [pywt.threshold(i, value=threshold, mode='hard') for i in coeffs[1:]]
         self.series = pywt.waverec(coeffs=coeffs, wavelet=wavelet, mode=mode)
+
         if len(series) % 2 != 0:
             return self.series[:-1]
         return self.series
+
+    def dwt_feature(self, series, threshold=1, mode='smooth', wavelet='sym8'):
+        new_coeffs = []
+        series = np.array(series).ravel()
+        self.level = pywt.dwt_max_level(self.series_len, wavelet)
+
+        # decompose time series
+        coeffs = pywt.wavedec(data=series, wavelet=wavelet, mode=mode, level=self.level)
+
+        threshold = threshold * (np.median(np.abs(coeffs[-1]))/0.6745) * np.sqrt(2 * np.log(len(coeffs[-1])))
+
+        # reconstruct cA
+        i = pywt.upcoef('a', coeffs[0], wavelet, level=len(coeffs)-1, take=len(series))
+        new_coeffs.append(i.tolist())
+
+        # reconstruct cD
+        for id, i in enumerate(coeffs[1:]):
+            i = pywt.threshold(i, value=threshold, mode="hard")
+            i = pywt.upcoef('d', i, wavelet, level=len(coeffs[1:])-id, take=len(series))
+
+            ## white noise test, h0 = white noise
+            # p_value = acorr_ljungbox(i, lags=[12], return_df=True)['lb_pvalue'].values[0] 
+            # if p_value < 0.05:
+            #     # print(id, 'not noise')
+            #     pass
+
+            new_coeffs.append(i.tolist())
+
+        return np.array(new_coeffs).T, np.sum(new_coeffs, axis=0)
+        return np.array(new_coeffs).T, self.dwt(series, threshold=threshold, wavelet=wavelet)
 
     def emd_transf(self, train_data):
         # generate IMFs from CEEMDAN decomposition
